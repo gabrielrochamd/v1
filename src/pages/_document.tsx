52e58bf29149c1ea2d6b3614d17de285cf3b90a0
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-01-03 22:46:24 -0300

    - Customize README - Add layout specified fonts and global styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700;900&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument

0c6a62aeeb3c4070b265b050f72b74874fb034f9
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-01-04 02:59:49 -0300

    Create Header

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/header.module.scss'
+
+export function Header() {
+  return (
+    <div className={styles.root}>
+      <ul>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+  )
+}

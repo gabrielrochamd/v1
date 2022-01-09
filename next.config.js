3be152715b7ba86b37a6a654e4ff486f2cba35cc
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-01-09 03:29:52 -0300

    Allow images from 'github.com'

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ['github.com']
+  },
   reactStrictMode: true,
   webpack: function(config) {
     config.module.rules.push({

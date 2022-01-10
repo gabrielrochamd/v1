4283728405a3ae78f535b6d4d273fa5e6b2b6096
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-01-10 01:07:30 -0300

    Change links order

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ export function Header() {
   return (
     <div className={styles.root}>
       <ul>
+        <li><a href="/blog">Blog</a></li>
         <li><a href="">Projects</a></li>
-        <li><a href="">Blog</a></li>
         <li><a href="">Contact</a></li>
       </ul>
     </div>

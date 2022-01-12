a54909f5519a74c9be06eef29865e43e731f7df5
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-01-12 01:18:48 -0300

    Create contact page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
       <ul>
         <li><a href="/blog">Blog</a></li>
         <li><a href="/projects">Projects</a></li>
-        <li><a href="">Contact</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </div>
   )
---
+++ src/pages/contact.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import styles from '../styles/pages/contact.module.scss'
+
+export default function Contact() {
+  return (
+    <div className={styles.root}>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <div className="container">
+        <h3>Contact</h3>
+        <form action="">
+          <div className={styles.inputContainer}>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" type="text" />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" rows={5} />
+          </div>
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  )
+}

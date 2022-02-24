bc9ea844a892dfbc6051556cb52a96da44b0d63c
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-02-24 01:46:12 -0300

    - Fix page not using loaded 'darkTheme' cookie - Use 'transitions' CSS class in all elements and adding it after first theme setting

--- src/contexts/DarkThemeContext.tsx
+++ src/contexts/DarkThemeContext.tsx
@@ -15,6 +15,7 @@ export const DarkThemeContext = createContext({} as ContextData)
 
 export function DarkThemeProvider({ children, ...rest }: Props) {
   const [active, setActive] = useState(rest.darkTheme)
+  const [firstChange, setFirstChange] = useState(true)
 
   useEffect(() => {
     const root = document.querySelector(':root')
@@ -23,8 +24,17 @@ export function DarkThemeProvider({ children, ...rest }: Props) {
     } else {
       root?.classList.remove('darkTheme')
     }
+    if (firstChange) {
+      root?.classList.add('transitions')
+      setFirstChange(false)
+    }
     setCookies('darkTheme', active ? '1' : '0', { secure: true })
   }, [active])
+
+  useEffect(() => {
+    const root = document.querySelector(':root')
+    root?.classList.remove('transitions')
+  }, [])
   
   return (
     <DarkThemeContext.Provider value={{ active, setActive }}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.scss'
 import { getCookie } from 'cookies-next'
-import App, { AppContext } from 'next/app'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 import { DarkThemeProvider } from '../contexts/DarkThemeContext'
 
-type Props = AppProps & { darkTheme: boolean }
-
-function MyApp({ Component, pageProps, darkTheme }: Props) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const darkThemeCookie = getCookie('darkTheme', { secure: true })
+  const darkTheme = darkThemeCookie === '1' ? true : false
+  
   return (
     <DarkThemeProvider darkTheme={darkTheme}>
       <Header />
@@ -16,16 +16,6 @@ function MyApp({ Component, pageProps, darkTheme }: Props) {
       <Footer />
     </DarkThemeProvider>
   )
-}
-
-MyApp.getInitialProps = async (context: AppContext) => {
-  const appProps = await App.getInitialProps(context)
-  const darkTheme = getCookie('darkTheme', { req: context.ctx.req })
-
-  return {
-    ...appProps,
-    darkTheme: darkTheme === '1' ? true : false
-  }
 }
 
 export default MyApp
